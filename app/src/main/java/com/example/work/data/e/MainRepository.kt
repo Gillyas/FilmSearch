3e1bb642c469bb3d413b49a443a15109fb6fb783
Gillyas <155540907+Gillyas@users.noreply.github.com>
2024-10-19 20:15:38 +0400

    перевели на флоу

--- app/src/main/java/com/example/work/data/e/MainRepository.kt
+++ app/src/main/java/com/example/work/data/e/MainRepository.kt
@@ -1,21 +1,16 @@
 package com.example.work.data.e
 
 
-import androidx.lifecycle.LiveData
-import com.example.work.data.e.dp.DatabaseHelper
 import com.example.work.data.e.Enity.Film
 import com.example.work.data.e.dao.FilmDao
-import java.util.concurrent.Executors
+import kotlinx.coroutines.flow.Flow
 
 class MainRepository(private val filmDao: FilmDao) {
 
     fun putToDb(films: List<Film>) {
-        //Запросы в БД должны быть в отдельном потоке
-        Executors.newSingleThreadExecutor().execute {
-            filmDao.insertAll(films)
-        }
+        filmDao.insertAll(films)
     }
-//ViewModel теперь этот метод будет вызываться один раз, чтобы взять ссылку на этот объект
-    fun getAllFromDB(): LiveData<List<Film>> = filmDao.gerCachedFilms()
+
+    fun getAllFromDB(): Flow<List<Film>> = filmDao.getCachedFilms()
 
 }
--- app/src/main/java/com/example/work/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/work/view/fragments/HomeFragment.kt
@@ -18,106 +18,82 @@ import com.example.work.view.rv_adapters.FilmListRecyclerAdapter
 import com.example.work.view.rv_adapters.TopSpacingItemDecoration
 import com.example.work.view.MainActivity
 import com.example.work.viewmodel.HomeFragmentViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.Locale
 
-
-@Suppress("UNREACHABLE_CODE")
 class HomeFragment : Fragment() {
     private val viewModel by lazy {
         ViewModelProvider.NewInstanceFactory().create(HomeFragmentViewModel::class.java)
     }
-    private lateinit var binding: FragmentHomeBinding
     private lateinit var filmsAdapter: FilmListRecyclerAdapter
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var scope: CoroutineScope
     private var filmsDataBase = listOf<Film>()
         //Используем backing field
         set(value) {
-            //Если придет такое же значение, то мы выходим из метода
+            //Если придет такое же значение то мы выходим из метода
             if (field == value) return
-            //Если пришло другое значение, то кладем его в переменную
+            //Если прило другое значение, то кладем его в переменную
             field = value
             //Обновляем RV адаптер
             filmsAdapter.addItems(field)
         }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
-        val lottieAnimationView: LottieAnimationView = binding.lottieAnim
-        lottieAnimationView.playAnimation()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.filmsListLiveData.observe(viewLifecycleOwner, Observer<List<Film>> {
-            filmsDataBase = it
-        })
+
         AnimationHelper.performFragmentCircularRevealAnimation(
             binding.homeFragmentRoot,
             requireActivity(),
             1
         )
-        binding.mainRecycler.apply {
-            //Инициализируем наш адаптер в конструктор передаем анонимно инициализированный интерфейс,
-            //оставим его пока пустым, он нам понадобится во второй части задания
-            filmsAdapter =
-                FilmListRecyclerAdapter { film ->
-                    (requireActivity() as MainActivity).launchDetailsFragment(
-                        film
-                    )
-                }
-            // тут создвли UX при нажатие на все поле происходит "активация"
-            binding.searchView.setOnClickListener {
-                binding.searchView.isIconified = false
-            }
-            //Подключаем слушателя изменений введенного текста в поиска
-            binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
-                androidx.appcompat.widget.SearchView.OnQueryTextListener {
-                //Этот метод отрабатывает при нажатии кнопки "поиск" на софт клавиатуре
-                override fun onQueryTextSubmit(query: String?): Boolean {
-                    return true
-                }
 
-                //Этот метод отрабатывает на каждое изменения текста
-                override fun onQueryTextChange(newText: String?): Boolean {
-                    //Если ввод пуст то вставляем в адаптер всю БД
-                    //Если ввод пуст то вставляем в адаптер всю БД
-                    if (newText!!.isEmpty()) {
-                        filmsAdapter.addItems(filmsDataBase)
-                        return true
-                    }
-                    //Фильтруем список на поискк подходящих сочетаний
-                    val result = filmsDataBase.filter {
-                        //Чтобы все работало правильно, нужно и запрос, и имя фильма приводить к нижнему регистру
-                        it.title.lowercase(Locale.getDefault())
-                            .contains(newText.lowercase(Locale.getDefault()))
-                    }
-                    //Добавляем в адаптер
-                    filmsAdapter.addItems(result)
-                    return true
-                }
-            })
-            viewModel.showProgressBar.observe(viewLifecycleOwner, Observer<Boolean> {
-                binding.progressBar.isVisible = it
-            })
-
-            //Присваиваем адаптер
-            adapter = filmsAdapter
-            //Присвои layoutmanager
-            layoutManager = LinearLayoutManager(requireContext())
-            //Применяем декоратор для отступов
-            val decorator = TopSpacingItemDecoration(8)
-            addItemDecoration(decorator)
-        }
-//Кладем нашу БД в RV
+        initSearchView()
+        initPullToRefresh()
+        //находим наш RV
+        initRecyckler()
         //Кладем нашу БД в RV
-        viewModel.filmsListLiveData.observe(viewLifecycleOwner, Observer<List<Film>> {
-            filmsDataBase = it
-            filmsAdapter.addItems(it)
-        })
+
+        scope = CoroutineScope(Dispatchers.IO).also { scope ->
+            scope.launch {
+                viewModel.filmsListData.collect {
+                    withContext(Dispatchers.Main) {
+                        filmsAdapter.addItems(it)
+                        filmsDataBase = it
+                    }
+                }
+            }
+            scope.launch {
+                for (element in viewModel.showProgressBar) {
+                    launch(Dispatchers.Main) {
+                        binding.progressBar.isVisible = element
+                    }
+                }
+            }
+        }
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        scope.cancel()
     }
 
     private fun initPullToRefresh() {
@@ -127,8 +103,59 @@ class HomeFragment : Fragment() {
             filmsAdapter.items.clear()
             //Делаем новый запрос фильмов на сервер
             viewModel.getFilms()
-            //Убираем крутящееся колечко
+            //Убираем крутящиеся колечко
             binding.pullToRefresh.isRefreshing = false
+        }
+    }
+
+    private fun initSearchView() {
+        binding.searchView.setOnClickListener {
+            binding.searchView.isIconified = false
+        }
+
+        //Подключаем слушателя изменений введенного текста в поиска
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            //Этот метод отрабатывает при нажатии кнопки "поиск" на софт клавиатуре
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            //Этот метод отрабатывает на каждое изменения текста
+            override fun onQueryTextChange(newText: String): Boolean {
+                //Если ввод пуст то вставляем в адаптер всю БД
+                if (newText.isEmpty()) {
+                    filmsAdapter.addItems(filmsDataBase)
+                    return true
+                }
+                //Фильтруем список на поискк подходящих сочетаний
+                val result = filmsDataBase.filter {
+                    //Чтобы все работало правильно, нужно и запроси и имя фильма приводить к нижнему регистру
+                    it.title.toLowerCase(Locale.getDefault())
+                        .contains(newText.toLowerCase(Locale.getDefault()))
+                }
+                //Добавляем в адаптер
+                filmsAdapter.addItems(result)
+                return true
+            }
+        })
+    }
+
+    private fun initRecyckler() {
+        binding.mainRecycler.apply {
+            filmsAdapter =
+                FilmListRecyclerAdapter { film ->
+                    (requireActivity() as MainActivity).launchDetailsFragment(
+                        film
+                    )
+                }
+            //Присваиваем адаптер
+            adapter = filmsAdapter
+            //Присвои layoutmanager
+            layoutManager = LinearLayoutManager(requireContext())
+            //Применяем декоратор для отступов
+            val decorator = TopSpacingItemDecoration(8)
+            addItemDecoration(decorator)
         }
     }
 
